8a6ec90a787dec0624590a43dfc643380c943ef0
Enrique Montemayor <eemontemayor@live.com>
2019-02-26 10:54:26 -0600

    wrote out psuedocode

--- datastore.js
+++ datastore.js
@@ -6,19 +6,36 @@ const STORE = [{
 function renderHtml(){
 
 }
+function scoreKeeper(){
+// this function will add +1 to score if questions is correct
+}
+function questionCounter(){
+// this function keeps track of the current question viewer is on
+}
+
+function giveFeedback(){
+    //this function will let viewer know if they got the question right
+}
 
 function handleSubmit(){
-
+// this function will listen submit
 }
 
 function verifyAnswer(){
-
+// this function will check the submitted answer to see if it is correct
 }
 
 function handleNextQuestion(){
-
+// this function will exit the feedback screen and go to next question
 }
 
 function startQuiz(){
-    
+// this function starts a new quiz
+}
+
+function main(){
+    handleNextQuestion();
+    handleSubmit();
+    renderHtml();
+    startQuiz();
 }
