8efd38804d02b0f38c3f62b911ae7bfa3a9c230b
Eddie Bueno <eddiebueno93@gmail.com>
2019-02-26 11:26:38 -0800

    added questions

--- datastore.js
+++ datastore.js
@@ -1,10 +1,38 @@
+'use strict';
+
 const STORE = {
-//  current question
-//  user's answer choices
-//  current view 
-//  Score
-}
-// // const QUESTIONS = [];
+  questionCounter: null,
+  usersAnswers: [],
+  currentView: 'start', 
+  score: 0,
+};
+
+const QUESTIONS = [
+  {
+    question: 'What does FPS stand for?',
+    answer1: 'flag per shot',
+    answer2: 'funky penguin singing',
+    answer3: 'first person shooter',
+    answer4: 'first player shooter',
+    correctAnswer: 'answer3',
+  },
+  {
+    question: 'What is the name of Link\'s horse in The Legend of Zelda Ocarina of Time?',
+    answer1: 'Percy',
+    answer2: 'Epona',
+    answer3: 'Navi',
+    answer4: 'Fred',
+    correctAnswer: 'answer2',
+  },{
+    question: 'What are the ghosts\' name in Pac-Man?',
+    answer1: 'Blinky, Pinky, Inky, and Clyde',
+    answer2: 'Blinky, Pinky, Winky, and Steve',
+    answer3: 'Blinky, Winky, Timmy, and Bob',
+    answer4: 'Blinky, Winky, Clyde, and Steve',
+    correctAnswer: 'answer1',
+  },
+];
+
 // function generateAnswerList(){
 
 // }
@@ -26,7 +54,7 @@ function questionCounter(){
 }
 
 function giveFeedback(){
-    //this function will let viewer know if they got the question right
+  //this function will let viewer know if they got the question right
 }
 
 function handleAnswerSubmited(){
@@ -46,8 +74,8 @@ function startQuiz(){
 }
 
 function main(){
-    handleNextQuestion();
-    handleSubmit();
-    renderHtml();
-    startQuiz();
+  handleNextQuestion();
+  handleSubmit();
+  renderHtml();
+  startQuiz();
 }
