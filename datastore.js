3b4913634eb66feeb5a0a82b1f11460246cab4bd
Enrique Montemayor <eemontemayor@live.com>
2019-02-26 10:48:21 -0600

    added js function stubs

---
+++ datastore.js
@@ -0,0 +1,24 @@
+const STORE = [{
+    currentQuestion:
+    answersCorrect:
+}]
+
+function renderHtml(){
+
+}
+
+function handleSubmit(){
+
+}
+
+function verifyAnswer(){
+
+}
+
+function handleNextQuestion(){
+
+}
+
+function startQuiz(){
+    
+}
