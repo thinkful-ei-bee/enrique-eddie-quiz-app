0b81840fe2281bb72aa65aea8e65c2f8a06bc544
Eddie Bueno <eddiebueno93@gmail.com>
2019-02-26 11:58:26 -0800

    created index.html, updated datastore, adjuested views for questions and start

--- datastore.js
+++ datastore.js
@@ -31,19 +31,20 @@ const QUESTIONS = [
     answer4: 'Blinky, Winky, Clyde, and Steve',
     correctAnswer: 'answer1',
   },
-
-{question: 'Should you ever attack a chicken in a Zelda game?',
-  answer1: 'Yes',
-  answer2: 'You can’t',
-  answer3: 'Only if you have a death wish!',
-  answer4: 'There are no chickens is these games',
-  correctAnswer: 'answer3',},
-  {question: 'Which Zelda game allows you to make your own food (and eat it, too!)?',
-  answer1: 'Ocarina of Time',
-  answer2: 'Majora’s Mask',
-  answer3: 'Skyward Sword',
-  answer4: 'Breath of the Wild',
-  correctAnswer: 'answer4',},
+  {question: 'Should you ever attack a chicken in a Zelda game?',
+    answer1: 'Yes',
+    answer2: 'You can’t',
+    answer3: 'Only if you have a death wish!',
+    answer4: 'There are no chickens is these games',
+    correctAnswer: 'answer3',},
+  {
+    question: 'Which Zelda game allows you to make your own food (and eat it, too!)?',
+    answer1: 'Ocarina of Time',
+    answer2: 'Majora’s Mask',
+    answer3: 'Skyward Sword',
+    answer4: 'Breath of the Wild',
+    correctAnswer: 'answer4',
+  },
 ];
 
 // function generateAnswerList(){
@@ -54,16 +55,43 @@ const QUESTIONS = [
 
 // }
 
-// function renderHtml(){
-// if (STORE.view === 'start'){
+function generateIntro(){
+  return `
+  <h1>Do you want to take this quiz?</h1>
+  <button type="button" class="startButton">Start the Quiz</button>
+  `
+}
 
-// } else if (STORE.view === 'quiz'){}
-// // }
+function generateQuiz(index){
+  const question = QUESTIONS[index];
+return `
+<p class="question-text">
+      ${question.question}
+    </p>
+    <form class="question-form">
+      <button class="question-1" type="radio">${question.answer1}</button>
+      <button class="question-1" type="radio">${question.answer2}</button>
+      <button class="question-2" type="radio">${question.answer3}</button>
+      <button class="question-3" type="radio">${question.answer4}</button>
+      <button class="question-4" type="submit">Submit</button>
+    </form>`
+}
+
+function renderHtml(){
+if (STORE.view === 'start'){
+  $('.intro').html(generateIntro());
+  $('.quiz').empty();
+} else if (STORE.view === 'quiz'){
+  $('.intro').empty();
+  $('.quiz').html(generateQuiz(STORE.questionCounter));
+}
+// }
 function scoreKeeper(){
 // this function will add +1 to score if questions is correct
 }
 function questionCounter(){
 // this function keeps track of the current question viewer is on
+  STORE.questionCounter++;
 }
 
 function giveFeedback(){
@@ -72,6 +100,7 @@ function giveFeedback(){
 
 function handleAnswerSubmited(){
 // this function will listen submit
+
 }
 
 function verifyAnswer(){
@@ -84,6 +113,11 @@ function handleNextQuestion(){
 
 function startQuiz(){
 // this function starts a new quiz
+  $('.intro').on('click','.startButton',function(event){
+    STORE.questionCounter = 0;
+    STORE.currentView = 'question';
+    renderHtml();
+  })
 }
 
 function main(){
